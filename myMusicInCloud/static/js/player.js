6fc83b024c7112d0ffe19d6ce3a7c93523ab9622
shashank singh <shashank.personal@gmail.com>
2011-12-05 09:17:32 +0530

    stripped down player.js into only thing that are requored now .. the music playig features are usppsoed to be rest api so need for them to be inside player.js

--- myMusicInCloud/static/js/player.js
+++ myMusicInCloud/static/js/player.js
@@ -7,45 +7,25 @@
 
 //This is the class that interact with the interface
 var player = new (function(){
-    jQuery.getJSON("/static/api/wiki.json",function(data){
-        $("#wikiControl").html("<h1>"+data[0]+"</h1>");  
-        config.log("dasdsada"+data[0]);
-    });
-	//Simulate a playlist
-	this.playList = ["Updating The List"];
-	jQuery.getJSON("/static/api/name.json",function(json){this.playlist=json;console.log(json); });
-    console.log(this.playlist)
-	//The song position
-	this.position = -1;
-	//The current volume
+    $("#wikiControl").html("<h1>"+jQuery.getJSON("/static/api/wiki.json")+"</h1>");  
+	
+    //The current volume
 	this.volume = 3;
 	//Status: 0:pause, 1:play
 	this.status = 1;
-	//Jump to the next or first song if the it is in the last position
+	
+    //Jump to the next or first song if the it is in the last position
 	this.nextSong = function(){
-		if(this.position+1 == this.playList.length)
-			this.position = 0;
-		else
-			this.position++;
-		//So, the reference to this class is not lost
-		var me = this;
-		$(".title").fadeOut(200, function(){
-			$(this).text(me.playList[me.position]);
-		}).fadeIn();
+		$(".title").fadeOut(200, function(){"updating"}).fadeIn();
 	}
-	//Jump to he previous or last song if it is in the first position
+	
+    //Jump to he previous or last song if it is in the first position
 	this.prevSong = function(){
-		if(this.position-1 < 0)
-			this.position = this.playList.length-1;
-		else
-			this.position--;
-		
-		var me = this;
 		$(".title").fadeOut(200, function(){
-			$(this).text(me.playList[me.position]);
 		}).fadeIn();
 	}
-	//Increase the volume in one point
+	
+    //Increase the volume in one point
 	this.volumeInc = function(){
 		if(this.volume +1 <= 3){
 			this.volume++;
@@ -55,7 +35,8 @@ var player = new (function(){
 			}).fadeIn(200);
 		}
 	}
-	//Decrease the volume in one point
+	
+    //Decrease the volume in one point
 	this.volumeDec = function(){
 		if(this.volume -1 > 0){
 			this.volume--;
@@ -65,7 +46,8 @@ var player = new (function(){
 			}).fadeIn(200);
 		}
 	}
-	//Toggle play & pause
+	
+    //Toggle play & pause
 	this.playPause = function(){
 		this.status = !this.status;
 		var me = this;
@@ -77,5 +59,6 @@ var player = new (function(){
 		}).fadeIn(200);
 	}
 });
+
 //Starts playing :P
 player.nextSong();
